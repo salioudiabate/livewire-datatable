4fb4b411c09b1da30d6911b2c4b16270c9c6bf8e
Saliou DIABATE <s.diabate@panelys.com>
2026-08-18 23:10:36 +0000

    Darken the inactive sort indicator for better visibility
    text-slate-300 was too faint to read at a glance; bumped to
    text-slate-400 (hover: slate-500), matching the neutral icon color
    already used everywhere else in the package (search, filter, export,
    select-caret icons).

--- resources/views/components/header-row.blade.php
+++ resources/views/components/header-row.blade.php
@@ -36,7 +36,7 @@
                             @endif
                         </svg>
                     @else
-                        <span class="flex flex-col -space-y-1 text-slate-300 transition-colors duration-150 group-hover:text-slate-400" aria-hidden="true">
+                        <span class="flex flex-col -space-y-1 text-slate-400 transition-colors duration-150 group-hover:text-slate-500" aria-hidden="true">
                             <svg class="h-2.5 w-2.5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" /></svg>
                             <svg class="h-2.5 w-2.5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
                         </span>
